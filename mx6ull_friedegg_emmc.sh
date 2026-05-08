601ebd539fe7e27dd7eca6b7bc62cc8026d8775c
Friedegg <nuoyuann@gmail.com>
2026-05-08 20:24:27 +0800

    Refactor defconfig and build script for clarity and organization

--- mx6ull_friedegg_emmc.sh
+++ mx6ull_friedegg_emmc.sh
@@ -3,8 +3,11 @@ export PATH=$PATH:/usr/local/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnuea
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 
+# 清除编译产物，准备全新编译
 make distclean
-# 注意：配置名字换成了正点原子的内核默认配置
+# 加载自定义配置到.config
+# 注意通过make menuconfig修改的配置可以保存到.config，注意需要指定ARCH和CROSS_COMPILE，否则可能会保存错误的配置
+# 想要固化可以make savedefconfig，生成一个新的defconfig文件，再粘贴到./arch/arm/configs/目录下
 make imx_friedegg_emmc_defconfig
 # 编译内核主体、设备树和全部模块
 # make -j4 all
